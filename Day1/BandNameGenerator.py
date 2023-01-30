073172e481c207087899b2e77a800f42303a7986
Rithish <rithishjakkireddy@gmail.com>
2023-01-30 14:04:26 +0530

    Day1

---
+++ Day1/BandNameGenerator.py
@@ -0,0 +1,5 @@
+print("Welcome to the Band Name Generator.")
+city_name = input("What's name of the city you grew up in?\n")
+pet_name = input("What is the name of your pet?\n")
+
+print("Band Name: " + c + " " + p)
