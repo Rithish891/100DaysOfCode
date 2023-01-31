d33e94dbeec04d9e4df230659f82fce445c68dce
Rithish <rithishjakkireddy@gmail.com>
2023-01-31 11:52:28 +0530

    Revert "Day1"
    This reverts commit fcfb5195cc5c2ab17804051ec01c105c7fb20a4e.

--- Day2/TipCalculator.py
+++ Day2/TipCalculator.py
@@ -3,5 +3,4 @@ bill_amount = float(input("What was the total bill?"))
 tip = int(input("What percentage tip would you like to give? 10, 12, or 15?"))
 people = int(input("How many people to split the bill?"))
 
-print(f"Each person should pay: "
-      f"{round((bill_amount + (bill_amount * tip) / 100) / people,2)}")
+print(f"Each person should pay: {round((bill_amount + (bill_amount * tip) / 100) / people,2)}")
