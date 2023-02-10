d07871a9ee553ea09d51ab90b5f7d6e85ca3a8ea
Rithish <rithishjakkireddy@gmail.com>
2023-02-10 15:24:39 +0530

    Day10

---
+++ Day10/Calculator.py
@@ -0,0 +1,63 @@
+import os
+
+print("""
+ _____________________
+|  _________________  |
+| | Pythonista   0. | |  .----------------.  .----------------.  .----------------.  .----------------. 
+| |_________________| | | .--------------. || .--------------. || .--------------. || .--------------. |
+|  ___ ___ ___   ___  | | |     ______   | || |      __      | || |   _____      | || |     ______   | |
+| | 7 | 8 | 9 | | + | | | |   .' ___  |  | || |     /  \     | || |  |_   _|     | || |   .' ___  |  | |
+| |___|___|___| |___| | | |  / .'   \_|  | || |    / /\ \    | || |    | |       | || |  / .'   \_|  | |
+| | 4 | 5 | 6 | | - | | | |  | |         | || |   / ____ \   | || |    | |   _   | || |  | |         | |
+| |___|___|___| |___| | | |  \ `.___.'\  | || | _/ /    \ \_ | || |   _| |__/ |  | || |  \ `.___.'\  | |
+| | 1 | 2 | 3 | | x | | | |   `._____.'  | || ||____|  |____|| || |  |________|  | || |   `._____.'  | |
+| |___|___|___| |___| | | |              | || |              | || |              | || |              | |
+| | . | 0 | = | | / | | | '--------------' || '--------------' || '--------------' || '--------------' |
+| |___|___|___| |___| |  '----------------'  '----------------'  '----------------'  '----------------' 
+|_____________________|
+""")
+
+
+def add(a, b):
+    return a + b
+
+
+def subtract(a, b):
+    return a - b
+
+
+def divide(a, b):
+    return a / b
+
+
+def multiply(a, b):
+    return a * b
+
+
+def calc():
+    operations = {"+": add, "-": subtract, "*": multiply, "/": divide}
+
+    n1 = int(input("Enter your first number... "))
+
+    for i in operations:
+        print(i)
+    in_calc = True
+
+    while in_calc:
+        sym = input("Pick an operation from above... ")
+        n2 = int(input("Enter your next number... "))
+        solution = operations[sym](n1, n2)
+
+        print(f"Your solution is: {solution}")
+
+        choice = input(f"Type 'y' to continue with {solution}, or type 'n' to start a new calculator... ").lower()
+
+        if choice == 'y':
+            n1 = solution
+        else:
+            in_calc = False
+            os.system('cls')
+            calc()
+
+
+calc()
